9e1f1638353dd192203ac57e731f0584d3a3c7d1
buzink <rjf.buzink@gmail.com>
2022-07-20 09:13:08 +0200

    add git to build

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+git pull
+docker stop denoniframe
 docker build -t html-server-image:v1 .
-docker run -d -p 80:80 html-server-image:v1
+docker run --rm --name denoniframe -d -p 80:80 html-server-image:v1
 curl localhost:80
